1b880e24d647aa305bc69dd13e8ebfd262e6f291
sepetchi <isepetci11@gmail.com>
2022-08-01 00:16:15 +0300

    devsFear is a subgroup of group10 and it is useful

---
+++ src/featureSepetchi1/firstClassForDevsfear.java
@@ -0,0 +1,7 @@
+package featureSepetchi1;
+
+public class firstClassForDevsfear {
+    public static void main(String[] args) {
+        System.out.println("I must learn how to use github");
+    }
+}
