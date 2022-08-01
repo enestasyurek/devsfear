90e9e089ba9a69739d6277636b92e118e0f9bec4
enestasyurek <tasyurekenes22@gmail.com>
2022-08-02 02:32:10 +0300

    ArrayList Constructor

--- src/Enes/ClassEx/Student.java
+++ src/Enes/ClassEx/Student.java
@@ -1,5 +1,8 @@
 package Enes.ClassEx;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
     public String name;
     public int age;
@@ -7,22 +10,26 @@ public class Student {
     public String email;
     public int grade;
 
-    public Student(String name, int age, String phone, String email, int grade) {
+    public ArrayList<String> kids;
+
+    public Student(String name, int age, String phone, String email, int grade, String... kids) {
         this.name = name;
         this.age = age;
         this.phone = phone;
         this.email = email;
         this.grade = grade;
+        this.kids = new ArrayList<>(Arrays.asList(kids));
     }
 
-    @Override
+
     public String toString() {
         return "Student{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
                 ", phone='" + phone + '\'' +
                 ", email='" + email + '\'' +
-                ", grade=" + grade +
+                ", grade=" + grade + + '\'' +
+                ", kids=" + kids +
                 '}';
     }
 }
--- src/Enes/ClassEx/Students.java
+++ src/Enes/ClassEx/Students.java
@@ -1,8 +1,18 @@
 package Enes.ClassEx;
 
+import java.util.ArrayList;
+import java.util.*;
+import java.util.Collections;
+
 public class Students {
     public static void main(String[] args) {
-        Student student1 = new Student("Enes", 20, "0555555555", "asd@gmail.com",7);
-        System.out.println(student1);
+        Student student1 = new Student("Enes", 20, "0555555555", "asd@gmail.com",7, "Enes", "Sefa");
+        //System.out.println(student1);
+        Student student2 = new Student("Güngör", 21, "0555555555", "zxc@gmail.com",8);
+
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.addAll(Arrays.asList(student1,student2));
+        System.out.println(students);
     }
 }
